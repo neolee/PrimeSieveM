d041ac2cfce94f53b64d0ad5549f5a4ce201fd15
Neo Lee <neo.lee@gmail.com>
2014-07-15 18:50:15 +0800

    Initial Commit

---
+++ PrimeSieveM/PrimeSieveM.h
@@ -0,0 +1,13 @@
+//
+//  PrimeSieveM.h
+//  PrimeSieveM
+//
+//  Created by Neo on 7/15/14.
+//  Copyright (c) 2014 Paradigm X. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PrimeSieveM : NSObject
+
+@end
---
+++ PrimeSieveM/PrimeSieveM.m
@@ -0,0 +1,13 @@
+//
+//  PrimeSieveM.m
+//  PrimeSieveM
+//
+//  Created by Neo on 7/15/14.
+//  Copyright (c) 2014 Paradigm X. All rights reserved.
+//
+
+#import "PrimeSieveM.h"
+
+@implementation PrimeSieveM
+
+@end
